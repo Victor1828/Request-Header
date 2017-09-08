ef995501fdbb7ddce0353133aa7452ec3a94d35e
Victor Vega <victorvega1828@gmail.com>
2017-09-08 15:34:26 -0500

    ip address prefix removed

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/whoami', function(req, res, next) {
-  res.json({ipadress: req.ip, language:req.acceptsLanguages()[0], software: req.useragent.platform + '; ' + req.useragent.os, browser: req.useragent.browser});
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  if (ip.substr(0, 7) == "::ffff:") {
+    ip = ip.substr(7)
+  }
+  var language = req.acceptsLanguages()[0];
+  var software = req.useragent.platform + '; ' + req.useragent.os;
+  var browser = req.useragent.browser;
+  res.json({ipadsress: ip, language: language, software: software, browser: browser});
 });
 
 module.exports = router;
