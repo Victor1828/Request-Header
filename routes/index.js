91bbf299810c32841e52dc6667e4a591ef6207c5
Victor Vega <victorvega1828@gmail.com>
2017-09-08 14:57:05 -0500

    route and json created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/api/whoami', function(req, res, next) {
+  res.json({ipadress: req.ip, language:req.acceptsLanguages()[0], software: req.useragent.platform + '; ' + req.useragent.os, browser: req.useragent.browser});
 });
 
 module.exports = router;
